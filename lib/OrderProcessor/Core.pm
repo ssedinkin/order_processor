7d92ea650ea0566816e8babfffb0f68e6bb4c6b4
Сединкин Сергей <serg3452@yandex.ru>
2021-07-08 23:44:37 +0400

    Написали метод OrderProcessor::Core::select

---
+++ lib/OrderProcessor/Core.pm
@@ -0,0 +1,39 @@
+package OrderProcessor::Core;
+
+=head1 NAME
+
+OrderProcessor::Core
+
+=head1 DESCRIPTION
+
+Модуль низкоуровневой работы с базой данных.
+! Сюда должны приходить только проверенные и легитимные запросы.
+! Если сюда придет запрос на дроп базы данных, то именно это и произойдёт.
+
+=cut
+
+use Carp;
+
+=head1 METHODS
+
+=head2 select
+
+Метод запроса в базу данных.
+
+! Внимание. Упадёт, потому что не реализовано.
+
+IN:
+    $order_id - id заказа в базе, положительное число
+
+OUT:
+    не реализовано, должен быть hashref
+
+=cut
+
+sub select {
+    my ( $order_id ) = @_;
+
+    confess 'not implemented';
+}
+
+1;
