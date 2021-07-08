2d0e1aa6cb5e1d217eaf9cc8950d959f1ed1c704
Сединкин Сергей <serg3452@yandex.ru>
2021-07-08 20:57:25 +0400

    Написали POD модуля OrderProcessor

--- lib/OrderProcessor.pm
+++ lib/OrderProcessor.pm
@@ -1,7 +1,39 @@
 package OrderProcessor;
+
+=head1 NAME
+
+OrderProcessor
+
+=head1 DESCRIPTION
+
+Подготавливает приложение:
+    - плагины
+    - конфиги
+    - сикреты
+    - роуты
+
+=cut
+
 use Mojo::Base 'Mojolicious', -signatures;
 
-# This method will run once at server start
+=head1 METHODS
+
+=head2 startup
+
+Подготовка приложения перед стартом.
+Возьмет секретный пароль из секретного конфига, пропишет его в secrets.
+Пропишет роуты в routes.
+
+! Внимание. Запустится 1 раз при старте приложения.
+
+IN:
+    $self - объект Mojolicious
+
+OUT:
+    не важен - используется в void-контексте
+
+=cut
+
 sub startup {
     my ( $self ) = @_;
 
